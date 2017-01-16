84347196deb677fb18a8a7c08610bcc2b5937dd0
hq <hq@hqs-iMac.local>
2017-01-16 06:48:20 -0800

    completed tutorial 11 and 12

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,37 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  //constructor() {
+  //  super();
+  //  this.goToStore = this.goToStore.bind
+//  }
+  goToStore(event) {
+    event.preventDefault();
+    console.log('Youre Bitch');
+      //grab text from box
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`)
+      //second transition from to/store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
   render() {
+
+
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2> Please Enter A store </h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={
+          getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit"> Visit Store </button>
       </form>
     )
   }
 }
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
+
 
 export default StorePicker;
